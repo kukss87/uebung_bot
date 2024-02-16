74cd45328fd14da244967b51cde3096172082c81
kukss87 <s.kukss87@gmail.com>
2024-02-16 13:43:59 +0100

    imports

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from config import TOKEN
-
 
 
 bot = Bot(token=TOKEN)
