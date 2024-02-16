a9c3c406ebb18ab4400a24fadcdaa43be51dd29d
kukss87 <s.kukss87@gmail.com>
2024-02-16 13:48:06 +0100

    Command Start

--- bot.py
+++ bot.py
@@ -6,6 +6,19 @@ from aiogram.types import Message
 
 from config import TOKEN
 
-
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f"Hello, {message.from_user.full_name}!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
