e6749638435c394cce935b24e2ba2ccc91c41785
kukss87 <s.kukss87@gmail.com>
2024-02-16 14:16:51 +0100

    Inline keyboard Callback Data

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from config import TOKEN
+from keyboards import create_inline_kb
+
+topics = ['perfekt', 'pronomen', 'verb', 'adjective', 'noun', 'adverb']
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
@@ -13,7 +16,22 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer(f"Hello, {message.from_user.full_name}!")
+    await message.answer(f'Hello, {message.from_user.full_name}!')
+    await message.answer('Send me command /text')
+
+
+@dp.message(Command('text'))
+async def text(message: Message):
+    await message.answer(text='Well done!',
+                         reply_markup=create_inline_kb(2, *topics))
+
+
+@dp.callback_query(F.data == 'perfekt')
+async def perfekt(call: CallbackQuery):
+    await call.message.answer(text='User goes to state: FSMPerfekt')
+    await call.message.answer(text='User get a bunch of lessons')
+
+    await call.answer()
 
 
 async def main():
